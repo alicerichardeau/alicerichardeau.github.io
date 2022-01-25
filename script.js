04b293c741b6ab6be455d932b4ca9cf31baa6e29
alrich-456 <alicerichardeau@outlook.com>
2022-01-25 21:12:47 +0100

    contact section

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-const square = document.querySelector('.square');
+// const square = document.querySelector('.square');
 
-window.addEventListener('scroll', () => {
-    const scrolled = window.pageYOffset;
-    const val = scrolled * 0.5;
-    square.style.transform = `translateX(${val * 0.45}%)`
-});
+// window.addEventListener('scroll', () => {
+//     const scrolled = window.pageYOffset;
+//     const val = scrolled * 0.5;
+//     square.style.transform = `translateX(${val * 0.45}%)`
+// });
