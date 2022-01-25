026aec316deaee792b12c81e571786a086db02c8
alrich-456 <alicerichardeau@outlook.com>
2022-01-25 10:58:42 +0100

    background image

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 
+const square = document.querySelector('.square');
 
+window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+    const val = scrolled * 0.5;
+    square.style.transform = `translateX(${val * 0.45}%)`
+});
